a0619e2c0193b96a7e79f37c2b07cf0bee7db832
Matthew Makai <makaimc@gmail.com>
2011-06-12 18:01:54 -0400

    first introduction question

---
+++ introduction/sum10to100/answer.clj
@@ -0,0 +1,14 @@
+(ns sum10to100
+	(:use clojure.test))
+
+(defn sumMinToMax [current, max, increment, vals-list]
+	(println current)
+	(conj vals-list current)
+	(if (> current max)
+		(and (println (reduce + vals-list))
+		current)
+		(sumMinToMax (+ current increment) max increment (conj vals-list current))))
+
+(defn sum10to100 []
+	(sumMinToMax 10 100 10 '(0)))
+
