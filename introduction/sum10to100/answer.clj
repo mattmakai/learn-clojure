1a2399b407a45d6747805901afe15f1a274a6269
Matthew Makai <makaimc@gmail.com>
2011-06-12 18:07:32 -0400

    Updated answer to go to 100 instead of 110.

--- introduction/sum10to100/answer.clj
+++ introduction/sum10to100/answer.clj
@@ -4,7 +4,7 @@
 (defn sumMinToMax [current, max, increment, vals-list]
 	(println current)
 	(conj vals-list current)
-	(if (> current max)
+	(if (>= current max)
 		(and (println (reduce + vals-list))
 		current)
 		(sumMinToMax (+ current increment) max increment (conj vals-list current))))
